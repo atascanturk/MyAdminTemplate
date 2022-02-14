3c91e5ec91617487c78e33b0c27fba77276b0397
atascanturk <atascanturk@gmail.com>
2022-02-14 20:45:18 +0300

    Update HomeController.cs

--- MyWebsite.MvcUI/Controllers/HomeController.cs
+++ MyWebsite.MvcUI/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MyWebsite.Business.Abstract;
+using MyWebsite.Core.Utilities.Security.Captcha.Google;
 using MyWebsite.MvcUI.Models;
 using Serilog;
 using System;
@@ -13,55 +14,14 @@ using System.Threading.Tasks;
 namespace MyWebsite.MvcUI.Controllers
 {
     public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
-        IMaintenanceCheckService _maintenanceCheckService;        
-
-        public HomeController(ILogger<HomeController> logger, IMaintenanceCheckService maintenanceCheckService)
-        {
-            _logger = logger;
-            _maintenanceCheckService = maintenanceCheckService;
-        }
+    {      
 
         public IActionResult Index()
-        {
-            
-            var check = _maintenanceCheckService.Get(x => x.Id == 1);
-            if (check.IsUnderMaintenance == true)
-            {
-                if (check.EndTime > DateTime.Now)
-                {
-                    return View("ComingSoonIndex", new MaintenanceTimeViewModel { EndTime = check.EndTime });                   
-                }
-
-                else
-                {
-                    check.IsUnderMaintenance = false;
-                    check.EndTime = DateTime.Now;
-                    _maintenanceCheckService.Update(check);
-                    return View();
-                }
-               
-            } 
-            
+        {           
+                     
           return  View();
 
-        }
+        }      
 
-        public IActionResult ComingSoonIndex(MaintenanceTimeViewModel maintenanceTimeViewModel)
-        {
-            return View(maintenanceTimeViewModel);
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
